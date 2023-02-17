1cafc862a716dae3c2e490a654d7dbd0d114878e
felipemacedo315 <felipemacedo315@gmail.com>
2023-02-17 13:12:18 -0300

    add variavel de ambiente do MongoDb

--- configDB/conn.js
+++ configDB/conn.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 const URL_DATABASE = "mongodb://127.0.0.1:27017/produtos";
 
 mongoose.set("strictQuery", true);
-mongoose.connect(URL_DATABASE);
+mongoose.connect(process.env.MONGO_URL);
 
 module.export = mongoose;
