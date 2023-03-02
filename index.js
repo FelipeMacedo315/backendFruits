08194bcc0fbfe0591951bf72815e5fbbb1888610
felipemacedo315 <felipemacedo315@gmail.com>
2023-03-02 12:24:17 -0300

    add o total de itens nas respostas

--- index.js
+++ index.js
@@ -10,39 +10,48 @@ const cerealsModel = require("./model/cereals");
 
 const PORT = process.env.PORT || 3000;
 // Send all fruits
+
 app.get("/DishoApi/fruits", (req, res) => {
   const limit = 10;
   const page = req.query.page;
+  var totalProducts;
+  const count = fruitsModel.find({}).then((result) => (totalProducts = result.length));
   fruitsModel
     .find({})
     .limit(limit)
     .skip((page - 1) * limit)
     .then((data) => {
-      res.status(200).json({ currentPage: page, products: data });
+      res.status(200).json({ currentPage: page, totalItems: totalProducts, products: data });
     })
     .catch((err) => console.log(err));
 });
+
 app.get("/DishoApi/vegetables", (req, res) => {
   const limit = 10;
   const page = req.query.page;
+  var totalProducts;
+  const count = vegetablesModel.find({}).then((result) => (totalProducts = result.length));
   vegetablesModel
     .find({})
     .limit(limit)
     .skip((page - 1) * limit)
     .then((data) => {
-      res.status(200).json({ currentPage: page, products: data });
+      res.status(200).json({ currentPage: page, totalItems: totalProducts, products: data });
     })
     .catch((err) => console.log(err));
 });
+
 app.get("/DishoApi/cereals", (req, res) => {
   const limit = 10;
   const page = req.query.page;
+  var totalProducts;
+  const count = cerealsModel.find({}).then((result) => (totalProducts = result.length));
   cerealsModel
     .find({})
     .limit(limit)
     .skip((page - 1) * limit)
     .then((data) => {
-      res.status(200).json({ currentPage: page, products: data });
+      res.status(200).json({ currentPage: page, totalItems: totalProducts, products: data });
     })
     .catch((err) => console.log(err));
 });
