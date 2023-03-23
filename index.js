8f389e5f96e56ab37a54ac44d0a46ecf21f4506f
FelipeMacedo315 <79103248+FelipeMacedo315@users.noreply.github.com>
2023-03-23 10:11:58 -0300

    tentando reverter o commit de cors melhorado

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const vegetablesController = require("./controllers/vegetables");
 const cerealsController = require("./controllers/cereals");
 const usersController = require("./controllers/users");
 const PORT = process.env.PORT || 3000;
-
-app.use(fruitsController);
-app.use(vegetablesController);
-app.use(cerealsController);
-app.use(usersController);
 app.use(
   cors({
     origin: ["http://localhost:8080", "https://api-disho.up.railway.app/DishoApi/"],
   })
 );
+app.use(fruitsController);
+app.use(vegetablesController);
+app.use(cerealsController);
+app.use(usersController);
 
 app.listen(PORT, () => {
   console.log("Server is ON in port:" + PORT);
